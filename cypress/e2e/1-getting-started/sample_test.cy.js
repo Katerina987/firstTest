f302083011facc1933423d5bb5df12cc0742f09a
Katerina987 <katerina.mickoska@climedo.de>
2024-11-05 09:02:47 +0100

    homework 2

--- cypress/e2e/1-getting-started/sample_test.cy.js
+++ cypress/e2e/1-getting-started/sample_test.cy.js
@@ -1,6 +1,6 @@
 describe('Sample Website', () => {
     it('should load the homepage and check the title', () => {
-        cy.visit('https://https://qa-1.climedo.de');
-        cy.title().should('include', 'qa-1');
+        cy.visit('https://qa-1.climedo.de');
+        cy.url().should('include', 'qa-1.climedo.de');
     });
 });
